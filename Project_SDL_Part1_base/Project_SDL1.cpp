9275d261c135a01cdb646d8dd651fd684ac28e4f
Kentin Ly <kentin.ly@epita.fr>
2021-10-23 11:07:37 +0200

    [move à faire]

--- Project_SDL_Part1_base/Project_SDL1.cpp
+++ Project_SDL_Part1_base/Project_SDL1.cpp
@@ -1,20 +1,15 @@
-﻿// SDL_Test.cpp: Definiert den Einstiegspunkt für die Anwendung.
-//
-
-#include "Project_SDL1.h"
-
+﻿#include "Project_SDL1.h"
+#include <vector>
 #include <algorithm>
 #include <cassert>
 #include <cstdlib>
 #include <numeric>
 #include <random>
 #include <string>
-
 void init() {
   // Initialize SDL
   if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO) < 0)
     throw std::runtime_error("init():" + std::string(SDL_GetError()));
-
   // Initialize PNG loading
   int imgFlags = IMG_INIT_PNG;
   if (!(IMG_Init(imgFlags) & imgFlags))
@@ -22,82 +17,141 @@ void init() {
                              "SDL_image Error: " +
                              std::string(IMG_GetError()));
 }
-
-
-//
-
-
-void application::window()
-{
-    auto window_ptr = SDL_CreateWindow("MOUTON ET LOUP",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        frame_width, frame_height,
-        0);
-    if (!window_ptr)
-        throw std::runtime_error(std::string(SDL_GetError()));
-    std::cout << "Window create!" << std::endl;
-    
-    //
-    auto window_surface_ptr = SDL_GetWindowSurface(window_ptr);
-    if (!window_surface_ptr)
-        throw std::runtime_error(std::string(SDL_GetError()));
-    std::cout << "Surface create!" << std::endl;
-
-    SDL_FillRect(window_surface_ptr, NULL, SDL_MapRGB(window_surface_ptr->format, 0, 255, 0));
-}
-
-
-
-void application::handleEvents()
-{
-    auto start = SDL_GetTicks();
-
-    SDL_Event e;
-    bool quit = false;
-    while (!quit && (SDL_GetTicks() - start < 10000)) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-                break;
-            }
-        }
-    }
-}
-
-void application::clean()
-{
-    SDL_FreeSurface(window_surface_ptr_);
-    SDL_DestroyWindow(window_ptr_);
-    IMG_Quit();
-    SDL_Quit();
-    std::cout << "Game Cleaned " << std::endl;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
 namespace {
 // Defining a namespace without a name -> Anonymous workspace
 // Its purpose is to indicate to the compiler that everything
 // inside of it is UNIQUELY used within this source file.
+SDL_Texture* load_texture_for(const std::string& path, 
+                              SDL_Renderer* renderer) {
+SDL_Texture* loaded_image = IMG_LoadTexture(renderer, path.c_str());
+  if (loaded_image == nullptr) {
+    throw std::runtime_error("Unable to load image");
+  } 
+  return loaded_image;
+}
+int get_random_int_in_range(int min, int max) {
+  std::random_device rd;
+  std::mt19937 rng(rd());
+  std::uniform_int_distribution<int> uni(min, max);
+  return uni(rng);
+}
+}
+ground::ground(SDL_Renderer* window_renderer_ptr_)
+{
+  this->window_renderer_ptr_ = window_renderer_ptr_;
+  // this->background_=background_;
+}
+void ground::add_animal(animal* animal)
+  {
+    this->ListAnimal.push_back(animal);
+  };
 
-SDL_Surface* load_surface_for(const std::string& path,
-                              SDL_Surface* window_surface_ptr) {
-return 0 ;
-  // Helper function to load a png for a specific surface
-  // See SDL_ConvertSurface
-    }
-} // namespace
+void ground::update() // Fonction update Animaux
+{
+  SDL_Rect background = { 0, 0, frame_width, frame_height};
+  SDL_RenderFillRect(this->window_renderer_ptr_, &background);
+  for(auto animal : this->ListAnimal)
+  {
+    animal->move();
+  }
+}
+
+application::application(unsigned n_sheep, unsigned n_wolf) // config de la fenetre
+{
+  SDL_CreateWindowAndRenderer(frame_width, frame_height, 0, &this->window_ptr_, &this->window_renderer_ptr_);
+  SDL_SetRenderDrawColor(this->window_renderer_ptr_, 0, 255, 0, 100);
+  SDL_Rect background = { 0, 0, frame_width, frame_height};
+  SDL_RenderFillRect(this->window_renderer_ptr_, &background);
+  this->groundApp = new ground(this->window_renderer_ptr_);
+  for (unsigned i = 0; i < n_sheep;i++)
+  {
+    this->groundApp->add_animal(new sheep(this->window_renderer_ptr_));
+
+  }
+
+  for (unsigned j = 0; j < n_wolf; j++)
+     this->groundApp->add_animal(new wolf(this->window_renderer_ptr_));
+  SDL_RenderPresent(this->window_renderer_ptr_); // equivalent a SDL_Update
+}
+int application::loop(unsigned period)
+{
+  period = period * 1000; // Conversion des secondes en tick
+  while(SDL_GetTicks() < period)
+  {
+    this->groundApp->update();
+    SDL_Delay(frame_time);
+    SDL_RenderPresent(this->window_renderer_ptr_);
+  }
+  return 1;
+}
+application::~application()
+{
+  SDL_DestroyRenderer(this->window_renderer_ptr_);
+  SDL_DestroyWindow(this->window_ptr_);
+}
+animal::animal(const std::string& file_path, SDL_Renderer* window_renderer_ptr) {
+  // todo: The constructor has to load the sdl_surface that corresponds to the
+  // texture
+    this->animal_text = load_texture_for(file_path, window_renderer_ptr);
+    SDL_QueryTexture(this->animal_text, NULL, NULL, &this->animal_rect.w, &this->animal_rect.h);
+    this->animal_rect.x = get_random_int_in_range(frame_boundary, frame_width - frame_boundary);
+    this->animal_rect.y = get_random_int_in_range(frame_boundary, frame_height - frame_boundary);
+  };
+sheep::sheep(SDL_Renderer* window_renderer_ptr_):animal("../media/sheep.png",window_renderer_ptr_)
+{
+  SDL_Rect rect = this->getAnimalRect();
+  this->setWindow_renderer_ptr(window_renderer_ptr_);
+  SDL_RenderCopy(window_renderer_ptr_, this->getAnimalText(), NULL, &rect);
+}
+void sheep::move()
+{
+  SDL_Rect rect = this->getAnimalRect();
+  if (rect.x + frame_boundary >= frame_width)
+    this->set_r(false);
+  else if (rect.x - frame_boundary + rect.w <= 0)
+    this->set_r(true);
+  if (rect.y + frame_boundary >= frame_height)
+    this->set_d(false);
+  else if (rect.y - frame_boundary + rect.h <= 0)
+    this->set_d(true);
+  if (this->get_r())
+    rect.x += 1;
+  else
+    rect.x -= 1;
+  if(this->get_d())
+    rect.y += 1;
+  else
+    rect.y -= 1;
+  this->setAnimalRect(rect);
+  SDL_RenderCopy(this->getWindow_renderer_ptr(), this->getAnimalText(), NULL, &rect);
+}
+sheep::~sheep() = default;
+wolf::wolf(SDL_Renderer* window_renderer_ptr_):animal("../media/wolf.png",window_renderer_ptr_)
+{
+  SDL_Rect rect = this->getAnimalRect();
+  this->setWindow_renderer_ptr(window_renderer_ptr_);
+  SDL_RenderCopy(window_renderer_ptr_, this->getAnimalText(), NULL, &rect);
+}
+void wolf::move()
+{
+  SDL_Rect rect = this->getAnimalRect();
+  if (rect.x + frame_boundary >= frame_width)
+    this->set_r(false);
+  else if (rect.x - frame_boundary + rect.w <= 0)
+    this->set_r(true);
+  if (rect.y + frame_boundary >= frame_height)
+    this->set_d(false);
+  else if (rect.y - frame_boundary + rect.h <= 0)
+    this->set_d(true);
+  if (this->get_r())
+    rect.x += 1;
+  else
+    rect.x -= 1;
+  if(this->get_d())
+    rect.y += 1;
+  else
+    rect.y -= 1;
+  this->setAnimalRect(rect);
+  SDL_RenderCopy(this->getWindow_renderer_ptr(), this->getAnimalText(), NULL, &rect);
+}
+wolf::~wolf() = default;
--- Project_SDL_Part1_base/main.cpp
+++ Project_SDL_Part1_base/main.cpp
@@ -2,91 +2,28 @@
 #include <stdio.h>
 #include <string>
 
+int main(int argc, char* argv[]) {
 
-//application* app = nullptr;
+  std::cout << "Starting up the application" << std::endl;
 
-int main(int argc, char* argv[]) 
-{
-   /* init();
-    application* app = new application();
-    
-    app->window();
-    app->handleEvents();
-    app->clean();
-    */
-   
+  if (argc != 4)
+    throw std::runtime_error("Need three arguments - "
+                             "number of sheep, number of wolves, "
+                             "simulation time\n");
 
-    //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-        throw std::runtime_error(std::string(SDL_GetError()));
+  init();
 
-    //Initialize PNG loading
-    int imgFlags = IMG_INIT_PNG;
-    if (!(IMG_Init(imgFlags) & imgFlags))
-        throw std::runtime_error("SDL_image could not initialize! "
-            "SDL_image Error: " + std::string(IMG_GetError()));
+  std::cout << "Done with initilization" << std::endl;
 
-    auto window_ptr = SDL_CreateWindow("MOUTON ET LOUP",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        frame_width, frame_height,
-        0);
+  application my_app(std::stoul(argv[1]), std::stoul(argv[2]));
 
-    if (!window_ptr)
-        throw std::runtime_error(std::string(SDL_GetError()));
-    //
-    auto window_surface_ptr = SDL_GetWindowSurface(window_ptr);
+  std::cout << "Created window" << std::endl;
 
-    if (!window_surface_ptr)
-        throw std::runtime_error(std::string(SDL_GetError()));
+  int retval = my_app.loop(std::stoul(argv[3]));
 
-    SDL_FillRect(window_surface_ptr, NULL, SDL_MapRGB(window_surface_ptr->format, 0, 255, 0));
+  std::cout << "Exiting application with code " << retval << std::endl;
 
-    auto sheep = IMG_Load("media/sheep.png");
-    auto wolf = IMG_Load("media/wolf.png");
+  SDL_Quit();
 
-   if (!sheep)
-       throw std::runtime_error("Could not load image");
-
-   if (!wolf)
-       throw std::runtime_error("Could not load image");
-
-
-
-   auto rect = SDL_Rect{ 0,0,194,259 };
-   auto rect1 = SDL_Rect{ 100,100,100,259 };
-   //auto rect2 = SDL_Rect{ 0,0,194,100 };
-
-   if (SDL_BlitSurface(sheep, NULL, window_surface_ptr, &rect))
-       throw std::runtime_error("Could not apply texture.");
-
-   if (SDL_BlitSurface(wolf, NULL, window_surface_ptr, &rect1))
-       throw std::runtime_error("Could not apply texture.");
-
-   //if (SDL_BlitSurface(sheep, NULL, window_surface_ptr, &rect2))
-     //  throw std::runtime_error("Could not apply texture.");
-
-    SDL_UpdateWindowSurface(window_ptr);
-
-    auto start = SDL_GetTicks();
-
-    SDL_Event e;
-    bool quit = false;
-    while (!quit && (SDL_GetTicks() - start < 10000)) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-                break;
-            }
-        }
-    }
-
-    SDL_FreeSurface(window_surface_ptr);
-    SDL_DestroyWindow(window_ptr);
-    IMG_Quit();
-    SDL_Quit();
-    std::cout << "Game Cleaned " << std::endl;
-
-
-    return EXIT_SUCCESS;
+  return retval;
 }
